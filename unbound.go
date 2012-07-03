a3a4eef717c78c7ddab8add18ed83f7132d5a771
Miek Gieben <miek@miek.nl>
2012-07-03 20:43:04 +0200

    Add some more helper functions

--- unbound.go
+++ unbound.go
@@ -299,3 +299,15 @@ func (u *Unbound) DebugLevel(d int) error {
 	i := C.ub_ctx_debuglevel(u.ctx, C.int(d))
 	return newError(int(i))
 }
+
+// AddTaRR calls AddTa, but allows to directly use an dns.RR
+// This method is not found in Unbound.
+func (u *Unbound) AddTaRR(ta dns.RR) error { return u.AddTa(ta.String()) }
+
+// DataAddRR calls DataAdd, but allows to directly use an dns.RR
+// This method is not found in Unbound.
+func (u *Unbound) DataAddRR(data dns.RR) error { return u.DataAdd(data.String()) }
+
+// DataRemoveRR calls DataRemove, but allows to directly use an dns.RR
+// This method is not found in Unbound.
+func (u *Unbound) DataRemoveRR(data dns.RR) error { return u.DataRemove(data.String()) }
