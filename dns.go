ff3b4a5b869bfdd37b4004edfde1972270dbe6d3
Miek Gieben <miek@miek.nl>
2012-07-05 09:10:49 +0200

    better

--- dns.go
+++ dns.go
@@ -4,14 +4,14 @@ import (
 	"dns"
 )
 
-// AddTaRR calls AddTa, but allows to directly use an dns.RR
-// This method is not found in Unbound.
+// AddTaRR calls AddTa, but allows to directly use an dns.RR.
+// This method is not found in Unbound.`
 func (u *Unbound) AddTaRR(ta dns.RR) error { return u.AddTa(ta.String()) }
 
-// DataAddRR calls DataAdd, but allows to directly use an dns.RR
+// DataAddRR calls DataAdd, but allows to directly use an dns.RR.
 // This method is not found in Unbound.
 func (u *Unbound) DataAddRR(data dns.RR) error { return u.DataAdd(data.String()) }
 
-// DataRemoveRR calls DataRemove, but allows to directly use an dns.RR
+// DataRemoveRR calls DataRemove, but allows to directly use an dns.RR.
 // This method is not found in Unbound.
 func (u *Unbound) DataRemoveRR(data dns.RR) error { return u.DataRemove(data.String()) }
