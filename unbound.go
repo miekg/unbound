e1919055c33405d6728bf386d4b3cd06832820b5
Miek Gieben <miek@miek.nl>
2013-01-31 10:31:07 +0100

    docs

--- unbound.go
+++ unbound.go
@@ -64,7 +64,7 @@ type Result struct {
 	Rr           []dns.RR // The RR encoded from the Data, Qclass, Qtype and Qname (not in Unbound)
 	CanonName    string   // Canonical name of result
 	Rcode        int      // Additional error code in case of no data
-	AnswerPacket *dns.Msg // Full answer packet 
+	AnswerPacket *dns.Msg // Full answer packet
 	HaveData     bool     // True if there is data
 	NxDomain     bool     // True if nodata because name does not exist
 	Secure       bool     // True if result is secure
